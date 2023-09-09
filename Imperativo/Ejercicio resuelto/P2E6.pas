230cafe44b65355be263b472defbde033cd5d482
Santiago Caiciia Massello <scaiciia@gmail.com>
2023-09-09 20:24:34 -0300

    add P2E6

---
+++ Imperativo/Ejercicio resuelto/P2E6.pas
@@ -0,0 +1,31 @@
+program P2E6;
+
+procedure convertirABinario(num: Integer);
+    begin
+        WriteLn(num);
+        if (num < 2) then begin
+            Write(num);            
+        end else begin
+            convertirABinario(num div 2);
+            Write(num mod 2);
+        end;
+    end;
+
+procedure leerNumero();
+    var
+        num: Integer;
+    begin
+        WriteLn('Ingrese el numero a convertir (0 para salir)');
+        ReadLn(num);
+        while (num <> 0) do begin
+            Write('Numero en binario: ');
+            convertirABinario(num);
+            WriteLn('');
+            WriteLn('Ingrese el numero a convertir (0 para salir)');
+            ReadLn(num);
+        end;
+    end;
+
+begin
+    leerNumero();
+end.
